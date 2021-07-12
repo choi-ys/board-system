d302279c664a0206c3e26c58ab968b3d78baca6c
choy-ys <project.log.062@gmail.com>
2021-07-12 19:26:35 +0900

    [2021-07-12]용석:Add core module layer project
     - common-util : pure kotlin or java utility project

---
+++ core-module/common-util/src/main/kotlin/io/board/commonutil/CommonUtilApplication.kt
@@ -0,0 +1,11 @@
+package io.board.commonutil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommonUtilApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommonUtilApplication>(*args)
+}
---
+++ core-module/common-util/src/test/kotlin/io/board/commonutil/CommonUtilApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.board.commonutil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonUtilApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
