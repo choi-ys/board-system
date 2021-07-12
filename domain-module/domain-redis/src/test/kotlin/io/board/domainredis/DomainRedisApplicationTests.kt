96afd7d28ea8e7cab3bcc284a367971f59c89a37
choy-ys <project.log.062@gmail.com>
2021-07-12 19:25:07 +0900

    [2021-07-12]용석:Add domain module layer project
     - domain-mysql
     - domain-redis
     - domain-service
     - repositories
       - member
       - post

---
+++ domain-module/domain-redis/src/test/kotlin/io/board/domainredis/DomainRedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.board.domainredis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DomainRedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
